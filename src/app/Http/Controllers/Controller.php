822834975f433185e5eabaecca0c437453d042c1
yuta-hoshino-spice <yuta_hoshino@spice-factory.co.jp>
2025-05-21 16:15:40 +0900

    test:最後に中身は//のみに戻す

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -4,5 +4,15 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function test(int $a): string
+    {
+        // 型違反: intを返すべきでないのに返す
+        return $a;
+    }
+
+    public function undefinedVar(): int
+    {
+        // 未定義変数を返す
+        return $undefined;
+    }
 }
