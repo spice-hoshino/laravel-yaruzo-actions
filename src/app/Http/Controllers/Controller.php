e9a1bbd1b3e53b3f6ecc2bc2ab47c28f1aa4d74b
yuta-hoshino-spice <yuta_hoshino@spice-factory.co.jp>
2025-05-21 16:21:23 +0900

    fix:phpstan対応

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -4,15 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    public function test(int $a): string
-    {
-        // 型違反: intを返すべきでないのに返す
-        return $a;
-    }
-
-    public function undefinedVar(): int
-    {
-        // 未定義変数を返す
-        return $undefined;
-    }
+    //
 }
